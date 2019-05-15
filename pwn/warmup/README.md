c59550566fb24d361ac642f9a3e4d14fef6d11b3
moratorium08 <moratorium08@gmail.com>
2019-05-15 15:40:06 +0900

    Add warmup heap chal

---
+++ pwn/warmup/README.md
@@ -0,0 +1,28 @@
+# Typical Note System
+
+## 作問者
+
+@moratorium08
+
+## 問題文
+
+わいわい
+
+```
+nc hoge
+```
+
+## 配布
+
+* [tns](problem/warmup)
+* [libc-2.27.so](problem/libc-2.27.so)
+
+## 解く人向け
+
+見られることが想定されているファイルは、problem以下のファイルと、このREADMEだけです
+
+writeupディレクトリには問題の元となったプログラムや僕のwriteupがあるので注意で
+
+## 難易度想定
+
+Warmup
---
+++ pwn/warmup/writeup/tns.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void bye()
+{
+    exit(-1);
+}
+
+#define SIZE 10
+
+void *ptrs[10];
+
+int get_index() {
+    int index;
+    if (scanf("%d", &index) != 1)
+        bye();
+    if (0 <= index && index < SIZE) return index;
+    bye();
+    return 0;
+}
+
+void command_malloc() {
+    int index = get_index();
+    unsigned long long size;
+    if (scanf("%llu", &size) != 1)
+        bye();
+
+    char *buf = malloc(size);
+    if (buf == NULL) bye();
+
+    if (scanf("%s", buf) != 1) bye();
+    ptrs[index] = buf;
+}
+
+void command_free() {
+    int index = get_index();
+    free(ptrs[index]);
+}
+
+void command_show() {
+    int index = get_index();
+    printf("%s\n", ptrs[index]);
+}
+
+int main(void) {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stdin, NULL, _IONBF, 0);
+    alarm(30);
+
+    int choice = 0;
+    puts("Hello. Typical note system.");
+
+    while(1) {
+        if (scanf("%d", &choice) != 1)
+            bye();
+
+        switch(choice) {
+            case 0:
+                command_malloc();
+            case 1:
+                command_free();
+            case 2:
+                command_show();
+            default:
+                bye();
+        }
+    }
+    return 0;
+}
+
