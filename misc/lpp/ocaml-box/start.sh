08a497afe5331b5450f4a2adb73a160992af8ce4
moratorium08 <moratorium08@gmail.com>
2019-05-17 17:21:02 +0900

    Complete lpp

--- misc/lpp/ocaml-box/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-stdbuf -i0 -o0 -e0 /home/user/runtime/ocamlrun /home/user/ocaml -nostdlib -nopervasives
---
+++ misc/lpp/problem/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#stdbuf -i0 -o0 -e0 /ocaml/runtime/ocamlrun /ocaml/ocaml -nostdlib -nopervasives
+/ocaml/runtime/ocamlrun /ocaml/ocaml -nostdlib -nopervasives
