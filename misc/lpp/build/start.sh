6219525e69fb45605bcba894055aff620f67aa9f
moratorium08 <moratorium08@gmail.com>
2019-05-17 16:19:26 +0900

    Add ocaml-box

---
+++ misc/lpp/build/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+stdbuf -i0 -o0 -e0 /home/user/ocaml/runtime/ocamlrun /home/user/ocaml/ocaml -nostdlib -nopervasives
---
+++ misc/lpp/ocaml-box/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+stdbuf -i0 -o0 -e0 /home/user/runtime/ocamlrun /home/user/ocaml -nostdlib -nopervasives
